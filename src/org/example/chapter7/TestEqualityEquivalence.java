e63ea594f22a46d92fed43fe89e14f1a85578d45
hq20051252 <hq20051252@163.com>
2015-04-22 14:44:32 +0800

    Equals and Equivalence

---
+++ src/org/example/chapter7/TestEqualityEquivalence.java
@@ -0,0 +1,18 @@
+package org.example.chapter7;
+
+public class TestEqualityEquivalence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+/*
+ * 测试用的类, 覆盖了equals()方法.
+ */
+class TestEquals {
+	
+}
